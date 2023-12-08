39a9fd192e73d21af731dd59e6dbad7c7005609a
GridTrank <gggcccfff@outlook.com>
2023-12-08 18:41:12 +0800

    1

--- js/head.js
+++ js/head.js
@@ -4,11 +4,72 @@ class Head extends HTMLElement {
     constructor() {
         super()
         let h1=`
-        <div class="card" >
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="head_top f_row j_c_b">
+            <img src="./Elements/BB homepage_Blissful Brides logo.png" class="logo" />
+            <div class="head_right f_row">
+                <div class="">GET A QUOTE</div>
+            </div>
+        </div>
+        <div class="nav_list f_row">
+            <div class="nav_item">
+                <div class="dropdown">
+                    <div data-bs-toggle="dropdown">Dropdown button</div>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="nav_item">
+                <div class="dropdown">
+                    <div data-bs-toggle="dropdown">Dropdown button</div>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="nav_item">
+                <div class="dropdown">
+                    <div data-bs-toggle="dropdown">Dropdown button</div>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="nav_item">
+                <div class="dropdown">
+                    <div data-bs-toggle="dropdown">Dropdown button</div>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="nav_item">
+                <div class="dropdown">
+                    <div data-bs-toggle="dropdown">Dropdown button</div>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="nav_item">
+                <div class="dropdown">
+                    <div data-bs-toggle="dropdown">Dropdown button</div>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
         `
