c77f0f9c6c7d49d21fb5ccea7cffb00466d29c37
GridTrank <gggcccfff@outlook.com>
2023-12-09 17:05:35 +0800

    1

--- js/head.js
+++ js/head.js
@@ -3,77 +3,107 @@
 class Head extends HTMLElement {
     constructor() {
         super()
-        let h1=`
-        <div class="head_top f_row j_c_b">
-            <img src="./Elements/BB homepage_Blissful Brides logo.png" class="logo" />
-            <div class="head_right f_row">
-                <div class="">GET A QUOTE</div>
-            </div>
-        </div>
-        <div class="nav_list f_row">
-            <div class="nav_item">
-                <div class="dropdown">
-                    <div data-bs-toggle="dropdown">Dropdown button</div>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="nav_item">
-                <div class="dropdown">
-                    <div data-bs-toggle="dropdown">Dropdown button</div>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="nav_item">
-                <div class="dropdown">
-                    <div data-bs-toggle="dropdown">Dropdown button</div>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="nav_item">
-                <div class="dropdown">
-                    <div data-bs-toggle="dropdown">Dropdown button</div>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="nav_item">
-                <div class="dropdown">
-                    <div data-bs-toggle="dropdown">Dropdown button</div>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="nav_item">
-                <div class="dropdown">
-                    <div data-bs-toggle="dropdown">Dropdown button</div>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        `
+        let h1=` `
+
+        let headLabelActiveIndex=0
+        let headLabelList=[
+            {
+                label:'Banquet Price List',
+                child:[
+                    {c_label:'child',icon:'./Elements/user-icon.png'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                ]
+            },
+            {
+                label:'Wedding Events & Contests',
+                child:[
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                ]
+            },
+            {
+                label:'Promotion',
+                child:[
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                ]
+            },
+            {
+                label:'Trending Wedding Ideas',
+                child:[
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                ]
+            },
+            {
+                label:'Vendors',
+                child:[
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                    {c_label:'child'},
+                ]
+            },
+
+        ]
+        let labelHtml=''
+        for(var i=0;i<headLabelList.length;i++){
+            labelHtml+=`<div class= "label_item dropdown ${i===0?'active':''}" data-index=${i}>
+                    <div data-bs-toggle="dropdown"> ${headLabelList[i].label}</div>
+                    <div class="dropdown-menu">
+                        <div class="menu_list f_row">
+                    `
+            headLabelList[i].child.forEach((e)=>{
+                labelHtml+=`<div class="menu_item f_row ">
+                                <img src="${e.icon || ''}">
+                                ${e.c_label}
+                            </div>`
+            })
+            labelHtml+=` </div>  </div> </div>`
+        }
+        $('.label_list').append(labelHtml)
         $('#head').append(h1)
+
+
+
+        $('.label_item').click(function (e){
+            $(this).addClass('active')
+            $(this).siblings().removeClass('active')
+
+        })
+        $('.menu_item').click(function (e){
+            $(this).addClass('active')
+            $(this).siblings().removeClass('active')
+
+        })
     }
 
   
--- js/rem.js
+++ js/rem.js
@@ -1,13 +1,33 @@
-// (function (doc, win) {
-//     var docEl = doc.documentElement,
-//         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-//         recalc = function () {
-//             var clientWidth = docEl.clientWidth;
-            
-//             if (!clientWidth) return;
-//             docEl.style.fontSize = 16 * (clientWidth / 1920) + 'px';
-//         };
-//     if (!doc.addEventListener) return;
-//     win.addEventListener(resizeEvt, recalc, false);
-//     doc.addEventListener('DOMContentLoaded', recalc, false);
-// })(document, window);
+// rem等比适配配置文件
+
+setTimeout(() => {
+  const baseSize = 16
+  // 设置 rem 函数
+  function setRem() {
+    let width = document.documentElement.clientWidth
+    let num = 0
+    if (width <= 480) {
+      num = 375
+    } else if (width <= 980 && width > 480) {
+      num = 1220
+    } else if (width <= 1300 && width > 980) {
+      num = 1320
+    } else if (width <= 1600 && width > 1300) {
+      num = 1620
+    } else {
+      num = 1920
+    }
+    const scale = document.documentElement.clientWidth / num
+    // 设置页面根节点字体大小（“Math.min(scale, 2)” 指最高放大比例为2，可根据实际业务需求调整）
+    document.documentElement.style.fontSize = baseSize * Math.min(scale, 2) + 'px'
+
+  }
+  // 初始化
+  setRem()
+  // 改变窗口大小时重新设置 rem
+  window.onresize = function () {
+    setRem()
+  }
+
+}, 0);
+
